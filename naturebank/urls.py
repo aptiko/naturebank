26e1eb8e3b56d5218e4a0675fe0ee09565e33371
Antonis Christofides <antonis@antonischristofides.com>
2021-10-15 19:02:20 +0300

    Use modern urls.py syntax

--- naturebank/urls.py
+++ naturebank/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 from django.conf import settings
 from django.views.generic.base import RedirectView
 
@@ -17,8 +17,7 @@ species_list = {
     'extra_context': {}
 }
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^biotopes/$', views.BiotopeListView.as_view(), name='biotope_list'),
     url(r'^biotopes/d/(?P<pk>\d+)/$', views.BiotopeDetailView.as_view(),
         name='biotope_detail'),
@@ -26,12 +25,12 @@ urlpatterns = patterns(
         views.BiotopeDetailView.as_view(), name='biotope_detail'),
     url(r'^biotopes/b/(?P<pk>\d+)/$', views.BiotopeDetailBriefView.as_view(),
         name='biotope_brief'),
-    (r'^biotope_list_filter/(?P<filter_name>[^/]+)/$',
-     views.biotope_list_filter),
+    url(r'^biotope_list_filter/(?P<filter_name>[^/]+)/$',
+        views.biotope_list_filter),
     url(r'^species/$', views.SpeciesListView.as_view(), name='species_list'),
     url(r'^species/d/(?P<pk>\d+)/$', views.SpeciesDetailView.as_view(),
         name='species_detail'),
-    (r'^(?P<layer>[^/]+)/kml/$', views.kml, {}),
-    (r'^bound/$', views.bound, {}),
-    (r'^settlements/$', views.settlements_kml, {}),
-)
+    url(r'^(?P<layer>[^/]+)/kml/$', views.kml, {}),
+    url(r'^bound/$', views.bound, {}),
+    url(r'^settlements/$', views.settlements_kml, {}),
+]
--- naturebank_project/urls.py
+++ naturebank_project/urls.py
@@ -1,16 +1,15 @@
 from django.contrib.gis import admin
 from django.conf import settings
-from django.conf.urls import include, patterns
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
-    (r'^admin/', include(admin.site.urls)),
-    (r'', include('naturebank.urls')),
-    (r'', include('django.contrib.flatpages.urls')),
-)
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('naturebank.urls')),
+    url(r'', include('django.contrib.flatpages.urls')),
+]
 
 if settings.DEBUG:
     urlpatterns = static(settings.MEDIA_URL,
